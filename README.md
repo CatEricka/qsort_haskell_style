255b3569534c87c7b24e11d4c4a6b99d8a70ac9a
CatEricka <32029431+CatEricka@users.noreply.github.com>
2022-12-08 20:14:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Haskell style quick sort, but rust version
+
+Just for fun, do not use it in any projects.
---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn qsort<T: Ord + Clone>(list: &[T]) -> Vec<T> {
+    match list {
+        [] => vec![],
+        [x, xs @ ..] => {
+            let (lesser, greater): (Vec<_>, Vec<_>) =
+                xs.to_owned().into_iter().partition(|y| *y < *x);
+            [qsort(&lesser), vec![x.to_owned()], qsort(&greater)].concat()
+        }
+    }
+}
+
+#[cfg(test)]
+mod qsort_tests {
+    use super::*;
+
+    #[test]
+    fn it_just_works() {
+        let result = qsort(&[1, 2, 5, 6, 789213, 99, -293, 8, 1]);
+        assert_eq!(result, [-293, 1, 1, 2, 5, 6, 8, 99, 789213]);
+    }
+}
